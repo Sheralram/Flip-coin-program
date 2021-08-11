e6cdb0381a9263ee947aa0440a4d03e166dd9c11
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-11 17:30:26 +0530

    Welcome to Flip Coin Program

---
+++ src/com/ram/flipcoinprogram.java
@@ -0,0 +1,12 @@
+package com.ram;
+
+
+import java.util.Scanner;
+
+public class flipcoinprogram {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to flip coin Program");
+
+            }
+}
